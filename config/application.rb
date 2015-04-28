c8dcbf580a876a0bedde6f59640b196533f88c56
Roman Bugaian <bugaian.roman@gmail.com>
2015-04-28 16:27:30 +0300

    Commited

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module Rails4Example
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Kyiv'
+    config.secret_key = '5c04d96f4051512490d1ef45ad43a925f6cae4e807dc9dbb6ba47d1572783cf63f6ce43d6b7dea776dad30dd28738edd3c4b78db9db6e70e492b648d7fa50c9b'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
